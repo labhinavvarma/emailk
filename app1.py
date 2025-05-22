3b23f14cefd5582d2959169390d11c2c73f59ceb
L Abhinav Varma <67184999+labhinavvarma@users.noreply.github.com>
2025-05-21 20:52:34 -0400

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,23 @@
+@app.post("/send_test_email")
+def send_test_email(email_request: EmailRequest):
+    try:
+        sender = 'noreply-vkhvkm'
+        subject = email_request.subject
+        body = email_request.body
+        receivers = email_request.receivers
+        recipients = [email.strip() for email in receivers.split(",")]
+
+        msg = MIMEMultipart()
+        msg['Subject'] = subject
+        msg['From'] = sender
+        msg['To'] = ', '.join(recipients)
+        msg.attach(MIMEText(body, 'html'))
+
+        smtpObj = get_ser_conn(logger, env=env, region_name=region_nm, aplctn_cd="aedl", port=None, tls=True, debug=False)
+        smtpObj.sendmail(sender, recipients, msg.as_string())
+        smtpObj.quit()
+        logger.info("Email sent successfully.")
+        return {"status": "success", "message": "Email sent successfully."}
+    except Exception as e:
+        logger.error(f"Error sending email: {e}")
+        raise HTTPException(status_code=500, detail=f"Error sending email: {e}")
